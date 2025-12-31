627907cbc924ca2b7bb166e84089137334c79393
Vinicius <viniciusr.camargo@gmail.com>
2025-12-31 07:56:34 -0300

    Incluindo Mapas

--- src/screens/components/cards.js
+++ src/screens/components/cards.js
@@ -1,53 +1,55 @@
 export default function Cards() {
   return (
-    <div className="row">
-      <h2>Como podemos te ajudar</h2>
-      <div className="col-sm-4 mb-3 mb-sm-0">
-        <div className="card">
-          <div className="card-body">
-            <h5 className="card-title">Acolhimento e escuta fraterna</h5>
-            <p className="card-text">
-              - A Pastoral da Sobriedade oferece apoio espiritual e humano para
-              pessoas em situação de dependência química e suas famílias. -
-              Espaço seguro para partilha e fortalecimento da fé.
-            </p>
-            <p className="card-text">
-              - Espaço seguro para partilha e fortalecimento da fé.
-            </p>
+    <>
+      <h2 className="mt-5 text-center">Como podemos te ajudar</h2>
+      <div className="row mt-3 text-center mb-5">
+        <div className="col-sm-4 mb-3 mb-sm-0 ">
+          <div className="card altCard">
+            <div className="card-body">
+              <h5 className="card-title">Acolhimento e escuta fraterna</h5>
+              <p className="card-text">
+                - A Pastoral da Sobriedade oferece apoio espiritual e humano
+                para pessoas em situação de dependência química e suas famílias.
+                - Espaço seguro para partilha e fortalecimento da fé.
+              </p>
+              <p className="card-text">
+                - Espaço seguro para partilha e fortalecimento da fé.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="col-sm-4 ">
+          <div className="card altCard">
+            <div className="card-body">
+              <h5 className="card-title">
+                Grupos de autoajuda com reuniões semanais
+              </h5>
+              <p className="card-text">
+                Encontros comunitários que promovem a sobriedade e a
+                reintegração social.
+              </p>
+              <p className="card-text">
+                Dinâmicas e reflexões baseadas nos 12 passos da sobriedade.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="col-sm-4 ">
+          <div className="card altCard">
+            <div className="card-body">
+              <h5 className="card-title">Orientação e encaminhamento</h5>
+              <p className="card-text">
+                Apoio na busca por tratamento e acompanhamento em centros
+                especializados.
+              </p>
+              <p className="card-text">
+                Indicação de recursos da Igreja e da comunidade para fortalecer
+                o processo de recuperação.
+              </p>
+            </div>
           </div>
         </div>
       </div>
-      <div className="col-sm-4">
-        <div className="card">
-          <div className="card-body">
-            <h5 className="card-title">
-              Grupos de autoajuda com reuniões semanais
-            </h5>
-            <p className="card-text">
-              Encontros comunitários que promovem a sobriedade e a reintegração
-              social.
-            </p>
-            <p className="card-text">
-              Dinâmicas e reflexões baseadas nos 12 passos da sobriedade.
-            </p>
-          </div>
-        </div>
-      </div>
-      <div className="col-sm-4">
-        <div className="card">
-          <div className="card-body">
-            <h5 className="card-title">
-              Orientação e encaminhamento
-            </h5>
-            <p className="card-text">
-              Apoio na busca por tratamento e acompanhamento em centros especializados.
-            </p>
-            <p className="card-text">
-              Indicação de recursos da Igreja e da comunidade para fortalecer o processo de recuperação.
-            </p>
-          </div>
-        </div>
-      </div>
-    </div>
+    </>
   );
 }
