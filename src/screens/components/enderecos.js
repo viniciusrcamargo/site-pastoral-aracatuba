1a07e549d3418ef18ceeb5515441bd4eb9aceaf6
Vinicius <viniciusr.camargo@gmail.com>
2026-01-14 15:30:23 -0300

    Alterando os endereços

--- src/screens/components/enderecos.js
+++ src/screens/components/enderecos.js
@@ -9,6 +9,7 @@ export default function Enderecos() {
               <h5 className="card-title">Paróquia Sant'Ana - Araçatuba</h5>
               <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3719.8797955473265!2d-50.43594712385283!3d-21.196933579010096!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x949643fef11b5537%3A0xbd0098ce3c84b403!2sPar%C3%B3quia%20Sant&#39;Ana%20-%20Diocese%20Ara%C3%A7atuba!5e0!3m2!1spt-BR!2sbr!4v1768250199363!5m2!1spt-BR!2sbr" width="600" height="450" style={{border:0}} loading="lazy" referrerPolicy="no-referrer-when-downgrade" className="mapa"></iframe>
               <p>Rua Tenente Alcides Theodoro dos Santos S/N - Aviação</p>
+              <p>Reuniões: Todas as Quintas as 19:30h</p>
             </div>
           </div>
         </div>
@@ -20,6 +21,7 @@ export default function Enderecos() {
               </h5>
               <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3719.0620003504337!2d-50.42478592385192!3d-21.22938988008557!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94964178421eae9d%3A0x998e6ef15599e619!2sPar%C3%B3quia%20Divino%20Esp%C3%ADrito%20Santo!5e0!3m2!1spt-BR!2sbr!4v1768251223250!5m2!1spt-BR!2sbr" width="600" height="450" style={{border:0}}  loading="lazy" referrerPolicy="no-referrer-when-downgrade" className="mapa"></iframe>
               <p>Rua Araçatuba, 488 - Alvorada</p>
+              <p>Reuniões Todas as Segundas as 20h</p>
             </div>
           </div>
         </div>
@@ -29,6 +31,7 @@ export default function Enderecos() {
               <h5 className="card-title">Paróquia São Sebastião - Araçatuba</h5>
               <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3720.107857990671!2d-50.4589635238532!3d-21.187873878710292!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9496446c2bb9fae3%3A0xc24cd127d823a3fd!2zUGFyw7NxdWlhIFPDo28gU2ViYXN0acOjbw!5e0!3m2!1spt-BR!2sbr!4v1768251348037!5m2!1spt-BR!2sbr" width="600" height="450" style={{border:0}} loading="lazy" referrerpolicy="no-referrer-when-downgrade" className="mapa"></iframe>
               <p>Rua São Sebastião, 220 - Tv</p>
+              <p>Reuniões Todas os Sábados as 15:30h</p>
             </div>
           </div>
         </div>
@@ -60,6 +63,17 @@ export default function Enderecos() {
               <h5 className="card-title">Paróquia São José - Castilho</h5>
               <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3727.995858825848!2d-51.49035272386119!3d-20.87221716834139!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9490b711943f65b7%3A0x592c05815cb1f452!2sIgreja%20Matriz%20da%20Par%C3%B3quia%20S%C3%A3o%20Jos%C3%A9!5e0!3m2!1spt-BR!2sbr!4v1768251776735!5m2!1spt-BR!2sbr" width="600" height="450" style={{border:0}} loading="lazy" referrerpolicy="no-referrer-when-downgrade" className="mapa"></iframe>
               <p>Av. Dr. Getúlio Vargas, Nº 746-788</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="row mt-3 text-center mb-5">
+        <div className="col-sm-4 ">
+          <div className="card mapaCard">
+            <div className="card-body">
+              <h5 className="card-title">Capela São Vicente - Araçatuba</h5>
+              <iframe src="https://www.google.com/maps/embed?pb=!4v1768415043353!6m8!1m7!1s6DvmRVDOGLY6Dhtn9-xbnw!2m2!1d-21.15008413472364!2d-50.50287089127048!3f57.345845555471975!4f6.968771537775723!5f1.0713917813119094" width="600" height="450" style={{border:0}}  loading="lazy" referrerpolicy="no-referrer-when-downgrade" className="mapa"></iframe>
+              <p>Rua Dr. Carlos Carvalho Rosa, N° 973 - Vila Brasil</p>
             </div>
           </div>
         </div>
